a6421208058cf18c69085651b90086e89418c352
Mudassir824472 <160173704+Mudassir824472@users.noreply.github.com>
2026-07-02 02:34:37 +0500

    Fix main.dart with unified theme and proper setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,35 @@
-  import 'package:firebase_core/firebase_core.dart';
-  import 'package:flutter/material.dart';
-  import 'package:provider/provider.dart';
-  import 'package:flutter_eproject/Controllers/auth_controller.dart';
-  import 'Controllers/admin_controller.dart';
-import 'Utils/app_navigator.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_eproject/Controllers/auth_controller.dart';
+import 'package:flutter_eproject/Utils/app_theme.dart';
+import 'lib/Screens/Authentication/Views/splash_screen.dart';
 
-  void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(); // Uncomment when Firebase is set up
+  
+  runApp(const MyApp());
+}
 
-    runApp(const MyApp());
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  class MyApp extends StatelessWidget {
-    const MyApp({Key? key}) : super(key: key);
-
-    @override
-    Widget build(BuildContext context) {
-      return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => AuthController()..checkAuthStatus()),
-          ChangeNotifierProvider(create: (_) => AdminController()),
-        ],
-        child: MaterialApp(
-          title: 'BabyShop',
-          theme: ThemeData(primarySwatch: Colors.blue),
-          home: const RoleBasedNavigator(),
-          onGenerateRoute: AppNavigator.generateRoute,
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthController()..checkAuthStatus(),
         ),
-      );
-    }
+        // Add AdminController here when needed
+        // ChangeNotifierProvider(create: (_) => AdminController()),
+      ],
+      child: MaterialApp(
+        title: 'BabyShop',
+        theme: AppTheme.lightTheme,
+        home: const SplashScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
   }
+}
